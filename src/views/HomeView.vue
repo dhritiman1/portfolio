a19850268ccca848524bfdc28266e6e5c23befcc
dhritiman <100087905+dhritiman1@users.noreply.github.com>
2023-07-10 19:25:18 +0530

    :making container width fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,39 +3,42 @@
     <filter id="noiseFilter">
       <feTurbulence
         type="fractalNoise"
-        baseFrequency="6.29"
-        numOctaves="6"
+        baseFrequency="4.29"
+        numOctaves="4"
         stitchTiles="stitch"
       />
     </filter>
   </svg>
-  <div>
-    <router-link to="/" class="font-bold">dhritiman</router-link>
-  </div>
-  <div class="mt-12 font-medium">
-    <p>
-      hi i'm 20y/o a guy with a computer, currently in 3rd year of BCA. i'm
-      interested in exploring things related to computers and web. i'm also
-      interested in creating aesthetically pleasing and minimalistic interfaces.
-    </p>
+  <div class="w-full md:w-[46rem]">
+    <div>
+      <router-link to="/" class="font-bold">dhritiman</router-link>
+    </div>
+    <div class="mt-12 font-medium">
+      <p class="text-justify">
+        hi i'm 20y/o a guy with a computer, currently in 3rd year of BCA. i'm
+        interested in exploring things related to computers and web. i'm also
+        interested in creating aesthetically pleasing and minimalistic
+        interfaces.
+      </p>
 
-    <p class="my-3">i'm currently trying to get my life together.</p>
+      <p class="my-3">i'm currently trying to get my life together.</p>
 
-    <!-- <p>you can check my cv here.</p> -->
+      <!-- <p>you can check my cv <a href="">here</a>.</p> -->
 
-    <p>find me on:</p>
+      <p>find me on:</p>
 
-    <ul class="list-disc pl-7 mt-1">
-      <li v-for="(item, index) in LINKS" :key="index">
-        <a class="hover:underline underline-offset-2" :href="item.url">
-          {{ item.name }}
-        </a>
-      </li>
-    </ul>
-  </div>
-  <!-- <div class="mt-8">
+      <ul class="list-disc pl-7 mt-1">
+        <li v-for="(item, index) in LINKS" :key="index">
+          <a class="hover:underline underline-offset-2" :href="item.url">
+            {{ item.name }}
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- <div class="mt-8">
     <p class="font-bold">blogs</p>
   </div> -->
+  </div>
 </template>
 
 <script setup lang="ts">
